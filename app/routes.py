402f15b7ed90055ddba4b87bbe31afb52407a067
Tino Yamaliev <tino.yamaliev@gmail.com>
2022-06-21 15:24:30 +0300

    Create User Profile

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,11 @@ from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 
 
-
 @app.route('/logout')
 def logout():
     logout_user();
     return redirect(url_for('index'))
+
 
 @app.route('/')
 @app.route('/index')
@@ -30,6 +30,18 @@ def index():
     return render_template('index.html', title='Home Page', posts=posts)
 
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'},
+        {'author': user, 'body': 'Test post #3'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -45,7 +57,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(url_for('index'))
-    return render_template('login.html',  title='Home Page', form=form)
+    return render_template('login.html', title='Home Page', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
